7d291e6ccc2f6b2838425d095f853aaaf61af18b
Leonardo David Fariña Ozamis <65524519+LeonardoFarina@users.noreply.github.com>
2023-03-28 13:13:43 -0600

    Update interface on the server

--- server.c
+++ server.c
@@ -19,15 +19,25 @@
 // global pid 
 static _Atomic int pid_count = 0;
 volatile sig_atomic_t flagRun = 1;
+volatile sig_atomic_t algor = 0;
 // JobTr
 typedef struct JobTr{
     int sock_fd;                // socket
 } JobTr;
+
+// Client Connection and Server connection
+typedef struct Win{
+    WINDOW *input;              // input window 
+    WINDOW *output;             // output window
+} Win;
+
 // Client Connection and Server connection
 typedef struct Connection{
     int sock_fd;                // socket server
-    int new_socket;                // socket client
+    int new_socket;             // socket client
+    struct Win *win;             // windows
 } Connection;
+
 pthread_mutex_t cpu_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int create_server_socket(){
@@ -85,6 +95,7 @@ void *handle_connection(void *arg) {
     Connection *connection = (Connection *)arg;
     int sock_fd = connection->sock_fd;
     int new_socket = connection->new_socket;
+    struct Win *win = connection->win;
 
     while(true) {
         Job *job = malloc(sizeof(Job));
@@ -94,10 +105,19 @@ void *handle_connection(void *arg) {
         } 
         else{
             if(job->burst == 0 || job->priority == 0){
-                printf("Job is empty\n");
+                printf("Client desconect\n");
                 break;
             }
-            printf("Received job with burst = %d, priority = %d\n", job->burst, job->priority);
+            char message[100];
+            sprintf(message, "Received job with burst = %d, priority = %d\n", job->burst, job->priority);
+
+            werase(win->input);
+            waddch(win->output, '\n');   
+            waddstr(win->output, message);
+            wrefresh(win->output); 
+            mvwprintw(win->input, 0, 0, "Command: ");   
+
+            //printf("Received job with burst = %d, priority = %d\n", job->burst, job->priority);
 
             // here you need to add to the ready queue
 
@@ -118,6 +138,55 @@ void *handle_connection(void *arg) {
 	pthread_detach(pthread_self());
 }
 
+void *window_thread(void *arg) {
+    Win *window = (Win *)arg;
+    WINDOW *input = window->input;
+    WINDOW *output window->output;  
+    bool done = FALSE;  
+    char bufferWin[1024];
+    char buffer[1024];
+    
+    while(!done) {
+        mvwprintw(input, 0, 0, "Command: ");
+        if (wgetnstr(input, bufferWin, COLS - 4) != OK) {
+            break;
+        }
+
+        werase(input);
+        waddch(output, '\n');   /* result from wgetnstr has no newline */
+        waddstr(output, "Server");
+        waddstr(output, ": ");
+        waddstr(output, bufferWin);
+        wrefresh(output);
+        done = (*bufferWin == 4);  /* quit on control-D */
+
+        // Here you put the commands
+
+        if (strcmp(bufferWin, "exit") == 0) {  
+            sprintf(buffer, "%s\n", bufferWin);
+            //send(socketfd, buffer, strlen(buffer), 0);
+            break;
+        }
+        else if (strcmp(bufferWin, "help") == 0) {
+            //sprintf(buffer, "%s\n", bufferWin);
+            werase(input);
+            waddch(output, '\n');   /* result from wgetnstr has no newline */
+            waddstr(output, "Help");
+            waddstr(output, ": \n");
+            waddstr(output, "Command help");
+            wrefresh(output);
+        }
+         else {    
+            sprintf(buffer, "Command: %s\n", bufferWin);
+            // send(socketfd, buffer, strlen(buffer), 0);
+        }
+
+        bzero(bufferWin, 1024);
+    }
+    endwin();
+	pthread_detach(pthread_self());
+}   
+
 int main(int argc, char **argv) {
 
     if(argc != 2){
@@ -130,30 +199,24 @@ int main(int argc, char **argv) {
 	}
 
 	char *algorithm = argv[1];    
-
-/*
-    int server_fd, new_socket; // Socket descriptors
-    struct sockaddr_in address; // Client Address
-    int addrlen = sizeof(address);
-
-    server_fd = create_server_socket();
-    init_server_address(&address);
-    bind_socket_to_address(server_fd, address);
-    listen_for_incoming_connections(server_fd, 3);
-    accept_incoming_connection(server_fd, address, &new_socket, &addrlen);
-*/
     
     // cases for the different algorithms and compare the text
     if (strcmp(algorithm, "FIFO") == 0) {
         printf("FIFO\n");
+        algor = 1;
     } else if (strcmp(algorithm, "SJF") == 0) {
         printf("SJF\n");
+        algor = 2;
     } else if (strcmp(algorithm, "HPF") == 0) {
         printf("HPF\n");
+        algor = 3;
     } else if (strcmp(algorithm, "RR") == 0) {
         printf("RR\n");
+        algor = 4;
     } else {
         printf("Invalid algorithm\n");
+        algor = 5;
+        return 0;
     }
 
     int server_fd, new_socket;
@@ -179,6 +242,21 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    WINDOW *input, *output;
+    initscr();
+    cbreak();
+    echo();
+    input = newwin(1, COLS, LINES - 1, 0);
+    output = newwin(LINES - 1, COLS, 0, 0);
+    wmove(output, LINES - 2, 0);    /* start at the bottom */
+    scrollok(output, TRUE);    
+    Win *window = (Win *)arg;
+    window->input = input;
+    window->output = output;  
+
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, window_thread, (void*)window);
+
     while (flagRun) {
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
                                  (socklen_t *)&addrlen)) < 0) {
@@ -189,15 +267,15 @@ int main(int argc, char **argv) {
         Connection *connection = (Connection *)malloc(sizeof(Connection));
         connection->sock_fd = new_socket;
         connection->new_socket = server_fd;
+        connection->win = window;
         pthread_create(&thread_id, NULL, handle_connection, (void*)connection);
     }
 
-    //close(new_socket); // closing the connected socket
     shutdown(server_fd, SHUT_RDWR); // closing the listening socket
 
     return 0;
 }
 
-// desplegar el ready queue
 
-// agreagar ncurses y reutilizar la funcion req_list del proyecto de chat leo
+
+// desplegar el ready queue
